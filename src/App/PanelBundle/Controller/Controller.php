19b6525875a92203c46c520b85614cf047db912b
Rafał Mikołajun <rafal@mikoweb.pl>
2016-09-12 01:27:15 +0200

    app abstract controller

---
+++ src/App/PanelBundle/Controller/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the vSymfo CMF.
+ *
+ * website: www.mikoweb.pl
+ * (c) Rafał Mikołajun <rafal@mikoweb.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\PanelBundle\Controller;
+
+use vSymfo\Bundle\PanelBundle\Controller\Controller as PanelController;
+
+/**
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @package vSymfo CMF
+ * @subpackage PanelBundle_Controller
+ */
+abstract class Controller extends PanelController
+{
+}

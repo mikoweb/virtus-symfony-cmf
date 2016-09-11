3ac99f8ce128cb0d4daa36b6a92d99b0b80e74be
Rafał Mikołajun <rafal@mikoweb.pl>
2016-09-12 01:30:16 +0200

    users and groups fixtures

---
+++ src/App/PanelBundle/DataFixtures/Install/LoadGroupData.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the vSymfo CMF.
+ *
+ * website: www.mikoweb.pl
+ * (c) Rafał Mikołajun <rafal@mikoweb.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\PanelBundle\DataFixtures\Install;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use vSymfo\Bundle\UserBundle\Entity\Group;
+use vSymfo\Bundle\UserBundle\Entity\Role;
+use vSymfo\Core\DataFixtures\AbstractGroupFixture;
+use vSymfo\Core\Entity\GroupAbstract;
+
+/**
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @package vSymfo CMF
+ * @subpackage PanelBundle_DataFixtures_Install
+ */
+class LoadGroupData extends AbstractGroupFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var Group $user */
+        $user = $this->createGroup($manager, Group::NAME_USER, Role::ROLE_USER);
+        $this->setReference('Group.User', $user);
+        $user->setType(Group::TYPE_USER);
+
+        /** @var Group $admin */
+        $admin = $this->createGroup($manager, Group::NAME_ADMIN, Role::ROLE_ADMIN);
+        $this->setReference('Group.Admin', $admin);
+        $admin->setType(Group::TYPE_EMPLOYEE);
+
+        foreach ($manager->getRepository(Role::class)->findAll() as $role) {
+            $this->setReference('Role.' . $role->getRole(), $role);
+        }
+
+        $this->loadGroupRolesFromXml('group_roles.xml');
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createGroupEntity()
+    {
+        return new Group();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 40;
+    }
+}
---
+++ src/App/PanelBundle/DataFixtures/Install/LoadUserData.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the vSymfo CMF.
+ *
+ * website: www.mikoweb.pl
+ * (c) Rafał Mikołajun <rafal@mikoweb.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\PanelBundle\DataFixtures\Install;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use FOS\UserBundle\Model\UserManager;
+use vSymfo\Bundle\UserBundle\Entity\Group;
+use vSymfo\Bundle\UserBundle\Entity\User;
+
+/**
+ * @author Rafał Mikołajun <rafal@mikoweb.pl>
+ * @package vSymfo CMF
+ * @subpackage PanelBundle_DataFixtures_Install
+ */
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var UserManager
+     */
+    protected $userManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+        $this->userManager = $this->container->get('fos_user.user_manager');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->createUser('admin@test.local', 'admin', 'admin', 'Group.Admin');
+    }
+
+    /**
+     * @param string $email
+     * @param string $username
+     * @param string $pass
+     * @param string $groupReference
+     *
+     * @return User
+     */
+    public function createUser($email, $username, $pass, $groupReference)
+    {
+        /** @var User $user */
+        $user = $this->userManager->createUser();
+        $user->setEmail($email)
+            ->setPlainPassword($pass)
+            ->setUsername($username)
+            ->setEnabled(true)
+        ;
+
+        /** @var Group $group */
+        $group = $this->getReference($groupReference);
+        $user->addGroup($group);
+        $this->userManager->updateUser($user);
+
+        $this->setReference('User.' . $username, $user);
+
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 60;
+    }
+}
